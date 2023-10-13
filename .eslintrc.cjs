1f706b12a47d3f62bf0763880bff118b407e76bb
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-10-13 14:08:17 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'jsx-a11y', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
