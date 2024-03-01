4bb321befd77af5ab889268a5a31412e5f469657
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2024-03-01 09:42:53 +0200

    Finnish the documentation of CountryCodeSelector's common functions

--- src/CountryCodeSelector/common.tsx
+++ src/CountryCodeSelector/common.tsx
@@ -9,8 +9,8 @@ import { CountryType } from '../lib/countryCodeData';
 import Variant from '../types/Variant';
 
 /**
- * Creates a default filter options function for the CountryCodeSelector (see
- * the documentation for Autocomplete component's filterOptions prop).
+ * Creates a default filter options function for the CountryCodeSelector's
+ * Autocomplete component.
  * @see {@link https://mui.com/material-ui/api/autocomplete/#props}
  */
 export function createDefaultFilterOptions() {
@@ -22,8 +22,8 @@ export function createDefaultFilterOptions() {
 }
 
 /**
- * Default function for rendering the options in CountryCodeSelector (see the
- * documentation for Autocomplete component's renderOption prop )
+ * Default function for rendering the options in the CountryCodeSelector's
+ * Autocomplete component.
  * @param props The props to apply on the li element.
  * @param option The option to render.
  * @returns The option element.
@@ -40,6 +40,15 @@ export function defaultRenderOption(
   );
 }
 
+/**
+ * Returns a default function for rendering the input component of the
+ * CountryCodeSelector's Autocomplete component.
+ * @param label CountryCodeSelector component's label passed to the input
+ *    component.
+ * @param shrink Indicates whether to shrink the label or not.
+ * @param variant Variant of the TextField component to use.
+ * @see {@link https://mui.com/material-ui/api/autocomplete/#props}
+ */
 export function createDefaultRenderInput(
   label: string,
   shrink?: boolean,
@@ -61,6 +70,12 @@ export function createDefaultRenderInput(
   };
 }
 
+/**
+ * A default function for getting the option label string in the
+ * CountryCodeSelector's Autocomplete component.
+ * @param option The option for which the string is to be formed.
+ * @see {@link https://mui.com/material-ui/api/autocomplete/#props}
+ */
 export function defaultGetOptionLabel(option: CountryType) {
   return `${option.country} (${option.iso})`;
 }
